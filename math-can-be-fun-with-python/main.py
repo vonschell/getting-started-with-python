70e50439f4a1c2ed5b85e54bb67c7b7a0d76c7e3
Vonschell <vonschell02@gmail.com>
2025-02-19 08:18:06 +0900

    Math can be fun with python challenge

---
+++ math-can-be-fun-with-python/main.py
@@ -0,0 +1,12 @@
+#divide 60 by 23
+print(60 / 23)
+
+#multiply 20 times 3 plus 18
+print(20 * 3 + 18)
+
+#assign values to pay and expenses variables
+pay = 3800
+expenses = 2200
+
+#subtract expenses from pay and print results
+print(pay - expenses)
