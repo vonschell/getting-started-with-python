202322cea97d4470f3ce7e3dc5b1bb25dd76063f
Vonschell <vonschell02@gmail.com>
2025-02-20 09:51:23 +0900

    Starting my drawing app

---
+++ start-your-drawing-app/main.py
@@ -0,0 +1,26 @@
+import turtle as turtle
+
+turtle.pencolor("orange")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("red")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("yellow")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("orange")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("red")
+turtle.forward(100)
+turtle.right(60)
+
+turtle.pencolor("yellow")
+turtle.forward(100)
+turtle.right(60)
+
