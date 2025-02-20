f3f3d391c34485414921ff2df5b9c209e1124304
Vonschell <vonschell02@gmail.com>
2025-02-20 10:06:16 +0900

    Using modules to shift time

--- start-your-drawing-app/main.py
+++ start-your-drawing-app/main.py
@@ -1,4 +1,5 @@
 import turtle as turtle
+import datetime 
 
 turtle.pencolor("orange")
 turtle.forward(100)
@@ -24,3 +25,14 @@ turtle.pencolor("yellow")
 turtle.forward(100)
 turtle.right(60)
 
+#Get the current time
+current_time = datetime.datetime.now()
+
+#Print the date in 00/00/00 format
+print("Date (MM/DD/YY):", current_time.strftime("%m/%d/%y"))
+
+#Print the date in January, 1, 2025 format
+print("Date (Month, Day, Year):", current_time.strftime("%B, %d, %Y"))
+
+#Print the date with the current time
+print("Date and Time:", current_time)
