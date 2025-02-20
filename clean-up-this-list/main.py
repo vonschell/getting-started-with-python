c7ae0c8dd2d3e6dc88343cac9aa6244fbe985238
Vonschell <vonschell02@gmail.com>
2025-02-20 18:24:41 +0900

    Cleaning up this list

--- clean-up-this-list/main.py
+++ clean-up-this-list/main.py
@@ -1,1 +1,7 @@
 websites = ["www.google.com","www.youtube.com","www.dropbox.com","www.quora.com","www.reddit.com","www.instagram.com"]
+
+websites.pop(2)
+websites.insert(2, "www.facebook.com")
+websites.append("www.snapchat.com")
+
+print(websites)
