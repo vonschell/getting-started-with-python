488fb97bd52629f67d79da074c31429a2a61cf7f
Vonschell <vonschell02@gmail.com>
2025-02-20 09:03:07 +0900

    Making an overtime pay function

--- build-an-hours-worked-function/main.py
+++ build-an-hours-worked-function/main.py
@@ -1,10 +1,15 @@
 # Write function here
 def calculate_pay(hours_worked, pay_per_hour):
+    if hours_worked > 40:
+        overtime_hours = hours_worked - 40
+        regular_pay = 40 * pay_per_hour
+        overtime_pay = overtime_hours * pay_per_hour * 2
+        return regular_pay + overtime_pay
     return hours_worked * pay_per_hour
 
-# Worked 40 hours at $20 an hour
-print(calculate_pay(40,20))
-# Worked 50 hours at $20 an hour
-print(calculate_pay(50,20))
-# Worked 40 hours at $12 an hour
-print(calculate_pay(40,12))
+# Worked 36 hours at $18 an hour
+print(calculate_pay(36,18))
+# Worked 60 hours at $25 an hour
+print(calculate_pay(60,25))
+# Worked 20 hours at $8 an hour
+print(calculate_pay(20,8))
