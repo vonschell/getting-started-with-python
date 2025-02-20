7388b044dab87b10d2cacdef98da467160d84e32
Vonschell <vonschell02@gmail.com>
2025-02-20 09:22:11 +0900

    Created a monthly income calculator

--- build-an-hours-worked-function/main.py
+++ build-an-hours-worked-function/main.py
@@ -13,3 +13,12 @@ print(calculate_pay(36,18))
 print(calculate_pay(60,25))
 # Worked 20 hours at $8 an hour
 print(calculate_pay(20,8))
+
+def calculate_monthly_pay(wk_1_hours, wk_2_hours, wk_3_hours, wk_4_hours, pay_per_hour):
+    week1_pay = calculate_pay(wk_1_hours, pay_per_hour)
+    week2_pay = calculate_pay(wk_2_hours, pay_per_hour)
+    week3_pay = calculate_pay(wk_3_hours, pay_per_hour)
+    week4_pay = calculate_pay(wk_4_hours, pay_per_hour)
+    return week1_pay + week2_pay + week3_pay + week4_pay
+
+print(calculate_monthly_pay(40,50,35,40,50))
