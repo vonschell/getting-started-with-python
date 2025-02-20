3d4eddd332382009bffd2e4899df9232f7e51eee
Vonschell <vonschell02@gmail.com>
2025-02-20 18:41:34 +0900

    Updating the list by slicing

---
+++ updating-this-list/main.py
@@ -0,0 +1,12 @@
+names = ["Amy", "Jade", "Shannin", "Ulrich", "Ricky", "Kirby", "Sokka", "Katara", "Zuko", "Aang", "Toph", "Appa", "Momo", "Suki", "Mei", "Azula", "Iroh", "Hilda", "Nick", "Zinzi"]
+dinner_invitees = names[0:4]
+print(dinner_invitees)
+
+drinks_invitees = names[16:]
+print(drinks_invitees)
+
+hiking_invitees = names[10:14]
+print(hiking_invitees)
+
+walk_invitees = names[8]
+print(walk_invitees)
