512e3fbedcda7e47360c69cca632f9a46a5581ff
Vonschell <vonschell02@gmail.com>
2025-02-21 08:16:43 +0900

    Creating a contact list dictionary

--- create-a-contacts-list-dictionary/main.py
+++ create-a-contacts-list-dictionary/main.py
@@ -3,3 +3,11 @@ title = ["Marketing Coordinator", "Software Developer", "Sales Representative", 
 
 company_org = {}
 
+for i in range(len(people)):
+    person = people[i]
+    person_title = title[i]
+    company_org[person] = person_title
+
+print(company_org)
+
+
