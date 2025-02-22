6001fb20acb40ffa70e9d9d001db4102423a8a0b
Vonschell <vonschell02@gmail.com>
2025-02-22 10:22:55 +0900

    Converting file types

--- convert-file-types/main.py
+++ convert-file-types/main.py
@@ -1,2 +1,13 @@
 import json
+
 show_data = []
+
+# Open the television_shows.txt file in read mode
+with open("television_shows.txt", "r") as text_file:
+    for line in text_file:
+        show_data.append(line)
+
+# Open a new file called best_shows.json in write mode
+with open("best_shows.json", "w") as write_file:
+    json.dump(show_data, write_file)
+    
